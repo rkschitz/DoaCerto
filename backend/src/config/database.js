bb4f3647e5d1dd5b2de8b6c1ae27a90b908a6726
Ruhan <ruhankaio900@gmail.com>
2025-06-17 21:30:15 -0300

    fix connection db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,6 @@ class Database {
     }
 
     Init() {
-        if (process.env.NODE_ENV === 'production') {
             this.db = new Sequelize(process.env.DATABASE_URL, {
                 dialect: 'postgres',
                 dialectOptions: {
@@ -17,19 +16,7 @@ class Database {
                     }
                 }
             });
-        } else {
-            this.db = new Sequelize(
-                process.env.DB_NAME,
-                process.env.DB_USER,
-                process.env.DB_PASSWORD,
-                {
-                    host: process.env.DB_HOST,
-                    port: process.env.DB_PORT,
-                    dialect: 'postgres',
-                }
-            );
-        }
+        } 
     }
-}
 
 module.exports = new Database();
